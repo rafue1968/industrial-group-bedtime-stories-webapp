587ac4f5f63910685d84001b838b6e0817d99bea
Rafue Karim Islam <rafue1968@gmail.com>
2025-09-04 23:02:09 +0000

    Create page.jsx for My Library page

---
+++ src/app/my-library/page.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Loading from "../../components/Loading";
+import { onAuthStateChanged } from "firebase/auth";
+import { getDoc, doc } from "firebase/firestore";
+import { firestore, auth } from "../../../lib/firebase";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import NavigationBar from "@/components/NavigationBar";
+import SavedStoriesList from "@/components/SavedStoriesList";
+
+
+export default function Page() {
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+    const [userId, setUserId] = useState("");
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+            const snap = await getDoc(doc(firestore, "users", user.uid));
+            setUserId(user.uid);
+            // const role = snap.exists() ? snap.data().role : "user";
+            } else {
+            router.push("/");
+            }
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    }, []);
+    
+    if (loading) return <Loading loading={loading} />;
+    if (!userId) router.push("/")
+
+    return (
+        <>
+            <NavigationBar />
+            <SavedStoriesList userId={userId} />
+        </>
+    )
+        
+
+}
